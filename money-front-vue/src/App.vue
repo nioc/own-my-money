53742c9e29adea0b6c44cb576c93bdbcad77a062
nioc <dev@nioc.eu>
2018-06-29 23:07:35 +0200

    Add router and some links in navbar

--- money-front-vue/src/App.vue
+++ money-front-vue/src/App.vue
@@ -4,7 +4,26 @@
       <div class="navbar-brand">
         <router-link class="navbar-item" to="/"><i class="fa fa-money fa-fw"/>&nbsp;OwnMyMoney</router-link>
       </div>
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <router-link class="navbar-item" to="/accounts">Accounts</router-link>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link"><i class="fa fa-wrench fa-fw"/>&nbsp;System settings</a>
+          </div>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link"><i class="fa fa-user fa-fw"/>&nbsp;User</a>
+            <div class="navbar-dropdown is-right">
+              <a class="navbar-item"><i class="fa fa-user-circle fa-fw"/>&nbsp;Profile</a>
+              <hr class="navbar-divider">
+              <a class="navbar-item"><i class="fa fa-sign-out fa-fw"/>&nbsp;Logout</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
+    <router-view></router-view>
   </div>
 </template>
 
--- money-front-vue/src/main.js
+++ money-front-vue/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 import 'font-awesome/css/font-awesome.min.css'
 import 'bulma/css/bulma.css'
 
@@ -8,6 +9,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#money',
+  router,
   components: { App },
   template: '<App/>'
 })
