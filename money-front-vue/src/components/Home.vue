3895aed540e1138d92d96fec62cee3df9b05198d
nioc <dev@nioc.eu>
2018-10-25 00:48:46 +0200

    Add transactions table to homepage

--- money-front-vue/src/components/Home.vue
+++ money-front-vue/src/components/Home.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="container">
-  </div>
+  <section class="hero">
+    <div class="hero-head">
+      <breadcrumb
+        :items="[
+          { link: '/', icon: 'fa-home', text: 'Home', isActive: true }
+        ]">
+      </breadcrumb>
+    </div>
+    <div class="hero-body container">
+      <div class="box">
+        <h1 class="title">Transactions</h1>
+        <transactions v-bind:url="url"/>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import Config from './../services/Config'
+import Breadcrumb from '@/components/Breadcrumb'
+import Transactions from '@/components/Transactions'
 export default {
+  name: 'home',
+  components: {
+    Breadcrumb,
+    Transactions
+  },
+  data () {
+    return {
+      url: Config.API_URL + 'transactions{/id}'
+    }
+  }
 }
 </script>
