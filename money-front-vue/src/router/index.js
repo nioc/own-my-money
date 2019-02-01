1bf69ca7fd3a1f99adeb4ec432bf5aa82fb33ba6
nioc <dev@nioc.eu>
2019-02-01 23:08:43 +0100

    Lazy-load route components

--- money-front-vue/src/router/index.js
+++ money-front-vue/src/router/index.js
@@ -3,16 +3,7 @@ import Router from 'vue-router'
 import Accounts from '@/components/Accounts'
 import Account from '@/components/Account'
 import Login from '@/components/Login'
-import Profile from '@/components/Profile'
-import Patterns from '@/components/Patterns'
-import Users from '@/components/Users'
-import Categories from '@/components/Categories'
-import Categorie from '@/components/Category'
-import Mappings from '@/components/Maps'
-import Mapping from '@/components/Map'
 import Home from '@/components/Home'
-import Setup from '@/components/Setup'
-import About from '@/components/About'
 Vue.use(Router)
 
 export default new Router({
@@ -26,13 +17,13 @@ export default new Router({
     {
       path: '/setup',
       name: 'setup',
-      component: Setup,
+      component: () => import(/* webpackChunkName: "user-settings" */ '@/components/Setup'),
       meta: { title: 'Setup server' }
     },
     {
       path: '/about',
       name: 'about',
-      component: About,
+      component: () => import(/* webpackChunkName: "user-settings" */ '@/components/About'),
       meta: { title: 'About' }
     },
     {
@@ -56,58 +47,58 @@ export default new Router({
     {
       path: '/profile',
       name: 'profile',
-      component: Profile,
+      component: () => import(/* webpackChunkName: "user-settings" */ '@/components/Profile'),
       meta: { title: 'Profile' }
     },
     {
       path: '/patterns',
       name: 'patterns',
-      component: Patterns,
+      component: () => import(/* webpackChunkName: "user-settings" */ '@/components/Patterns'),
       meta: { title: 'Patterns' }
     },
     {
       path: '/users',
       name: 'users',
-      component: Users,
+      component: () => import(/* webpackChunkName: "system-settings" */ '@/components/Users'),
       meta: { title: 'Manage users' }
     },
     {
       path: '/maps',
       name: 'maps',
-      component: Mappings,
+      component: () => import(/* webpackChunkName: "system-settings" */ '@/components/Maps'),
       meta: { title: 'Edit maps' }
     },
     {
       path: '/maps/new',
       name: 'newMap',
-      component: Mapping,
+      component: () => import(/* webpackChunkName: "system-settings" */ '@/components/Map'),
       props: { isNew: true },
       meta: { title: 'Edit map :code' }
     },
     {
       path: '/maps/:code',
       name: 'map',
-      component: Mapping,
+      component: () => import(/* webpackChunkName: "system-settings" */ '@/components/Map'),
       props: { isNew: false },
       meta: { title: 'Edit map :code' }
     },
     {
       path: '/categories',
       name: 'categories',
-      component: Categories,
+      component: () => import(/* webpackChunkName: "system-settings" */ '@/components/Categories'),
       meta: { title: 'Edit categories and subcategories' }
     },
     {
       path: '/categories/:id',
       name: 'category',
-      component: Categorie,
+      component: () => import(/* webpackChunkName: "system-settings" */ '@/components/Category'),
       props: { isCategory: true },
       meta: { title: 'Edit categorie :id' }
     },
     {
       path: '/categories/:pid/subcategories/:id',
       name: 'subcategory',
-      component: Categorie,
+      component: () => import(/* webpackChunkName: "system-settings" */ '@/components/Category'),
       props: { isCategory: false },
       meta: { title: 'Edit subcategorie :id' }
     }
